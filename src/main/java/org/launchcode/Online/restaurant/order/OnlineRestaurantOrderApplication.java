9b550e2224f6f5f3c0895ffc179448eabb8c736a
Vineela Gottimukkala <vineela517@gmail.com>
2019-07-30 22:51:44 -0500

    Initial Commit

--- src/main/java/org/launchcode/Online/restaurant/order/OnlineRestaurantOrderApplication.java
+++ src/main/java/org/launchcode/Online/restaurant/order/OnlineRestaurantOrderApplication.java
@@ -3,11 +3,13 @@ package org.launchcode.Online.restaurant.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.run;
+
 @SpringBootApplication
 public class OnlineRestaurantOrderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlineRestaurantOrderApplication.class, args);
+		run(OnlineRestaurantOrderApplication.class, args);
 	}
 
 }
