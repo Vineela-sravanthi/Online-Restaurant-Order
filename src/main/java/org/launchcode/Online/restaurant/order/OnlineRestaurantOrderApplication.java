1178ef8b1f2754947a5d4b2121f76b4e43c881d3
Sai Sravanthi <sravantireddi@gmail.com>
2019-07-31 15:21:31 -0500

    Initial commit

--- src/main/java/org/launchcode/Online/restaurant/order/OnlineRestaurantOrderApplication.java
+++ src/main/java/org/launchcode/Online/restaurant/order/OnlineRestaurantOrderApplication.java
@@ -1,15 +1,16 @@
 package org.launchcode.Online.restaurant.order;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class OnlineRestaurantOrderApplication {
 
-	public static void main(String[] args) {
-		run(OnlineRestaurantOrderApplication.class, args);
+	public static void main(String[] args){
+		SpringApplication.run(OnlineRestaurantOrderApplication.class, args);
 	}
+
+
 
 }
